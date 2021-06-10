20b9c2ac2d9fc9b1eb93e9a0454c233ffb2f43ad
EKastensmidt <64220094+EKastensmidt@users.noreply.github.com>
2021-06-10 19:53:06 -0300

    Camera follow point between player and mouse

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private Transform target;
-
     [SerializeField] private float smoothSpeed = 0.125f;
     [SerializeField] private Vector3 offset = new Vector3(0, 0, -1);
+    [SerializeField] private GameObject player;
+    private Vector3 mousePosition;
+    private Vector3 playerPosition;
 
     void Start()
     {
@@ -16,7 +17,17 @@ public class CameraFollow : MonoBehaviour
 
     void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, target.position + offset, smoothSpeed);
+        //transform.position = Vector3.Lerp(transform.position, target.position + offset, smoothSpeed);
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        playerPosition = player.transform.position;
+        Vector3 newPos = FollowMouse(mousePosition, playerPosition);
+
+        transform.position = Vector3.Lerp(newPos, playerPosition + offset, smoothSpeed);
     }
 
+    private Vector3 FollowMouse(Vector3 a, Vector3 b) {
+        Vector3 c = (a + b)/2;
+        Vector3 newPos = new Vector3((b.x + c.x) / 2, (b.y + c.y) / 2, transform.position.z);
+        return newPos;
+    }
 }
