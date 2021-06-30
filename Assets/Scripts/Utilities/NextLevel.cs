8598cc8134ff97adfa7e09f547f7a79d7a440110
EKastensmidt <64220094+EKastensmidt@users.noreply.github.com>
2021-06-30 15:03:26 -0300

    SceneChange bugfix

--- Assets/Scripts/Utilities/NextLevel.cs
+++ Assets/Scripts/Utilities/NextLevel.cs
@@ -5,29 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
-    private string currScene;
     private void Start()
     {
-        currScene = SceneManager.GetActiveScene().name;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == 8)
         {
-            switch (currScene)
+            switch (SceneManager.GetActiveScene().name)
             {
                 case "Tutorial":
-                    currScene = "Level_1";
+                    SceneManager.LoadScene("Level_1");
                     break;
                 case "Level_1":
-                    currScene = "Level_2";
+                    SceneManager.LoadScene("Level_2");
                     break;
                 case "Level_2":
-                    currScene = "BossFight_1";
+                    SceneManager.LoadScene("BossFight_1");
                     break;
-            }           
-            SceneManager.LoadScene(currScene);
+            }
         }
     }
 }
