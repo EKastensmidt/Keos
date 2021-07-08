c7f3d1eceeff9b503e002db6e298cd8e6968f8a4
EKastensmidt <64220094+EKastensmidt@users.noreply.github.com>
2021-07-08 17:29:43 -0300

    Level2 Complete

---
+++ Assets/Scripts/Utilities/FlipScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipScript : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private float flipRate = 1;
+    private float flipCd;
+    private bool flag = false;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (flipCd <= Time.time)
+        {
+            if (flag) flag = false;
+            else flag = true;
+            spriteRenderer.flipX = flag;
+            flipCd = flipRate + Time.time;
+        }
+    }
+}
