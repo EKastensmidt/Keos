9a0b9d55b49bb847063603ffcac9c5719d50e82d
EKastensmidt <64220094+EKastensmidt@users.noreply.github.com>
2021-07-19 22:12:32 -0300

    BossFight Layout

---
+++ Assets/Scripts/Utilities/BossFightEnter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFightEnter : MonoBehaviour
+{
+    [SerializeField] private GameObject gate;
+    private Collider2D collider;
+    void Start()
+    {
+        collider = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            gate.transform.position = new Vector3(gate.transform.position.x, gate.transform.position.y - 2f, gate.transform.position.z);
+            collider.enabled = false;
+        }
+    }
+}
