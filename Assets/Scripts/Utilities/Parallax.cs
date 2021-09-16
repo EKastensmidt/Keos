e7c5b363fc8851fecf8de042edbf21f888ebfd93
EKastensmidt <64220094+EKastensmidt@users.noreply.github.com>
2021-09-16 14:48:43 -0300

    Parallax background effect

---
+++ Assets/Scripts/Utilities/Parallax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, stratPos;
+    [SerializeField] private GameObject cam;
+    [SerializeField] private float parallaxEffect;
+
+    void Start()
+    {
+        stratPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+    void FixedUpdate()
+    {
+        float dist = (cam.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(stratPos * dist, transform.position.y, transform.position.z);
+
+    }
+}
