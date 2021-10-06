0df5a7cb19a1b9d50855edabc5c65f2e3c172db6
EKastensmidt <64220094+EKastensmidt@users.noreply.github.com>
2021-10-06 19:45:58 -0300

    Level 6

---
+++ Assets/Scripts/Camera/CameraPixelChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class CameraPixelChange : MonoBehaviour
+{
+    private PixelPerfectCamera pp;
+    [SerializeField] private float pixels;
+    private bool isInTrigger = false;
+
+    void Start()
+    {
+        pp = GameObject.Find("Main Camera").GetComponent<PixelPerfectCamera>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Maguito")
+        {
+            isInTrigger = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Maguito")
+        {
+            isInTrigger = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (isInTrigger && pp.assetsPPU > 24) pp.assetsPPU--;
+        else if (!isInTrigger && pp.assetsPPU < pixels) pp.assetsPPU++;
+    }
+}
