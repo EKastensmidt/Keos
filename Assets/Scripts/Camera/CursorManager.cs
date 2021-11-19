77a23f0a296287c20e5133b38bfb5a9aea50352f
EKastensmidt <64220094+EKastensmidt@users.noreply.github.com>
2021-11-19 20:54:08 -0300

    cursor change

---
+++ Assets/Scripts/Camera/CursorManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    [SerializeField] private Texture2D cursorTexture;
+
+    private void Start()
+    {
+        Cursor.SetCursor(cursorTexture, new Vector2(cursorTexture.width / 2 ,cursorTexture.height / 2), CursorMode.ForceSoftware);
+    }
+}
