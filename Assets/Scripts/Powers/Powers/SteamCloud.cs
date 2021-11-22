bf6af87adc1425295b5678e894c9ae6c124a400a
EKastensmidt <64220094+EKastensmidt@users.noreply.github.com>
2021-11-21 22:58:59 -0300

    button fixes + secretlvl 1 fix

--- Assets/Scripts/Powers/Powers/SteamCloud.cs
+++ Assets/Scripts/Powers/Powers/SteamCloud.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class SteamCloud : Powers
 {
     private GameObject prefab;
+    private float cd = 0.2f;
 
     public SteamCloud() : base(Elements.fire, Elements.water,"Steam Cloud")
     {
+        cooldownTime = cd;
         prefab = Resources.Load("SteamCloud") as GameObject;
     }
 
